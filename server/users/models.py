7d7be8a9511d70019762fc6806aa622fefd63b47
cjdango <cjdango@gmail.com>
2019-04-24 18:37:41 +0800

    Set is_active defailts to True.

--- server/users/models.py
+++ server/users/models.py
@@ -10,7 +10,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=80, null=True, blank=True)
 
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     date_joined = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
