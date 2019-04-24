3e0ef49e463467f7561fcb2c06805ecf1de8ff0b
cjdango <cjdango@gmail.com>
2019-04-24 10:08:45 +0800

    Generate login-view component.

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatModule } from '../mat.module'
-import { SignupViewComponent } from './signup-view/signup-view.component'
+import { SignupViewComponent } from './signup-view/signup-view.component';
+import { LoginViewComponent } from '../login-view/login-view.component'
 
 @NgModule({
-  declarations: [SignupViewComponent],
+  declarations: [SignupViewComponent, LoginViewComponent],
   imports: [
     CommonModule,
     MatModule,
