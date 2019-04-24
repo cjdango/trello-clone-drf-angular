8b83cd90c988589533b7ca78f0146a18850972b7
cjdango <cjdango@gmail.com>
2019-04-24 13:01:01 +0800

    Connect PasswordResetConfirmViewComponent in routes.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupViewComponent } from './users/signup-view/signup-view.component';
 import { LoginViewComponent } from './users/login-view/login-view.component';
 import { PasswordResetViewComponent } from './users/password-reset-view/password-reset-view.component';
+import { PasswordResetConfirmViewComponent } from './users/password-reset-confirm-view/password-reset-confirm-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'signup', component: SignupViewComponent },
   { path: 'login', component: LoginViewComponent },
-  { path: 'forgot-password', component: PasswordResetViewComponent }
+  { path: 'forgot-password', component: PasswordResetViewComponent },
+  { path: 'reset-password/:uid/:token', component: PasswordResetConfirmViewComponent }
 ];
 
 @NgModule({
