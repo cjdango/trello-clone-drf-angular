5b603b88be21ab50745adf2749c217bd168b2a4a
cjdango <cjdango@gmail.com>
2019-04-24 15:39:30 +0800

    Add board model.

--- server/boards/models.py
+++ server/boards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=50)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='board_owned_set')
+    members = models.ManyToManyField(
+        get_user_model(), related_name='board_set')
+    is_archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
