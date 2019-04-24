a5895ea284342cce3bb8739249f749301b227325
cjdango <cjdango@gmail.com>
2019-04-24 15:20:23 +0800

    Create ResetPassToken model.

--- server/users/models.py
+++ server/users/models.py
@@ -22,3 +22,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f"{self.email}"
+
+
+class ResetPassToken(models.Model):
+    uid = models.CharField(max_length=256)
+    token = models.CharField(max_length=256)
