e8b13bb9f4093a3691d505874bb3e78f3db75a8a
cjdango <cjdango@gmail.com>
2019-04-24 10:41:07 +0800

    Connect login view to routes.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SignupViewComponent } from './users/signup-view/signup-view.component';
+import { LoginViewComponent } from './users/login-view/login-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
-  { path: 'signup', component: SignupViewComponent }
+  { path: 'signup', component: SignupViewComponent },
+  { path: 'login', component: LoginViewComponent }
 ];
 
 @NgModule({
--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import {  AppRoutingModule } from '../app-routing.module';
 import { MatModule } from '../mat.module'
 import { SignupViewComponent } from './signup-view/signup-view.component';
-import { LoginViewComponent } from '../login-view/login-view.component'
+import { LoginViewComponent } from './login-view/login-view.component'
 
 @NgModule({
   declarations: [SignupViewComponent, LoginViewComponent],
   imports: [
     CommonModule,
     MatModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ]
 })
 export class UsersModule { }
